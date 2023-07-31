b264871a2230055b52c6356deb6aab200c1369dd
Sahib61003m <sahib61003@gmail.com>
2023-08-01 02:00:38 +0530

    Updated Day1

---
+++ day1.py
@@ -0,0 +1,20 @@
+# 1.Introduction to the Course 
+    # Deep Learning help computers to behave and understand like human, it is the subfield of Artificial Intelligence.
+    # Deep Learning is a subset of Machine Learning, which is a subset of Artificial Intelligence.
+    # In this course I will be tested through self-assessment quizzes, code challenges and practical assignments.
+    # Not only this, I will be making many projects with the guidance of Instructors at Internshala.
+
+    # Neural networks are building blocks of Deep Learning models
+    # Neural networks are inspired by the human brain, which is made up of billions of neurons.
+
+
+# 2. Basics of Statistics
+    # Types of Data - Qualitative and Quantitive 
+    # Qualitative data is categorical data, which is further divided into two types - Nominal and Ordinal
+    # Examples: Gender (Male/Female), Color(Red/Green)
+    # Nominal refers to unordered values. Example: Gender, Nationality etc
+    # Ordinal refers to ordered values or ranked values. Example: Rating like not satisfied, satisfied, highly satsified etc
+    # Quantitive data is numerical data, which is further divided into two types - Discrete and Continuous.
+    # Examples: Age, Height, Weight etc
+    # Discrete data is countable data. Example: Number of students in a class, Number of cars in a parking lot etc
+    # Continuous data is measurable data. Example: Height, Weight etc
