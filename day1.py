30314896546bbc1242d96c27fa2f81df7b3452de
Sahib61003m <sahib61003@gmail.com>
2023-08-01 14:21:19 +0530

    Updated day 1

--- day1.py
+++ day1.py
@@ -9,12 +9,29 @@
 
 
 # 2. Basics of Statistics
-    # Types of Data - Qualitative and Quantitive 
-    # Qualitative data is categorical data, which is further divided into two types - Nominal and Ordinal
-    # Examples: Gender (Male/Female), Color(Red/Green)
-    # Nominal refers to unordered values. Example: Gender, Nationality etc
-    # Ordinal refers to ordered values or ranked values. Example: Rating like not satisfied, satisfied, highly satsified etc
-    # Quantitive data is numerical data, which is further divided into two types - Discrete and Continuous.
-    # Examples: Age, Height, Weight etc
-    # Discrete data is countable data. Example: Number of students in a class, Number of cars in a parking lot etc
-    # Continuous data is measurable data. Example: Height, Weight etc
+    # 2.1 Types of Data - Qualitative and Quantitive 
+        # Qualitative data is categorical data, which is further divided into two types - Nominal and Ordinal
+        # Examples: Gender (Male/Female), Color(Red/Green)
+        # Nominal refers to unordered values. Example: Gender, Nationality etc
+        # Ordinal refers to ordered values or ranked values. Example: Rating like not satisfied, satisfied, highly satsified etc
+        # Quantitive data is numerical data i.e can be measured numerically, which is further divided into two types - Discrete and Continuous.
+        # Examples: Age, Height, Weight etc
+        # Discrete data is countable data. Example: Number of students in a class, Number of cars in a parking lot etc
+        # Continuous data is measurable data. Example: Height, Weight etc
+    # 2.2 Types of Statistics - Descriptive Statistics and Inferential Statistics
+        # Descriptive Stats consists of methods of organizing and describing data  using tables, graphs etc
+        # Measures of Central Tendency -  Mean, Mode, Median
+        # Measures of Dispersion - Range, Variance, Standard Deviation
+        #Inferential Stats consists of methods that use sample results to help make decisions or predictions. LR, Decision Tree, Neural Networks are its examples or tools.
+    # 2.3 Describig data graphically 
+        # Histograms, Box Plots, Scatter Plots, Bar Graphs, Pie Charts etc
+        # Frequency Distribution for qualitative data lists all categories and no. of elements that belong to each of the categories.
+        # Frequency Distribution for quantative data lists all classes and number of values that belong to each class. Here we have grouped data.
+        # Symmetric graph - data is represented in perfect normal curve.
+        # Skewed graph - 
+            # Positively skewed graph - data is concentrated on the left side of the graph.
+            # Negatively skewed graph - data is concentrated on the right side of the graph.
+    # Mesures  of Central Tendency
+        # Mean - Average of all values; Mean = Sum of all values / Number of values
+        # Median - Middle value of all values = 
+
